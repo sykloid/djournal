ca06131187a908c0378592c1a17ca38b0cbaf859
P.C. Shyamshankar <sykora@lucentbeing.com>
2010-09-05 13:41:55 +0530

    Define entry model.

---
+++ djournal/models.py
@@ -0,0 +1,20 @@
+'''Models for Djournal.'''
+
+from django.contrib.auth.models import User
+from django.db import models
+
+class Entry(models.Model):
+    '''Represents a Djournal entry.'''
+
+    title = models.CharField(max_length=128)
+    subtitle = models.CharField(max_length=128, blank=True)
+
+    slug = models.SlugField(max_length=128, unique=True)
+
+    author = models.ForeignKey(User, related_name='entries')
+
+    created = models.DateTimeField()
+    modified = models.DateTimeField()
+
+    teaser = models.TextField(blank=True)
+    body = models.TextField(blank=True)
