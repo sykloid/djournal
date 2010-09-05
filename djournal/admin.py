683c0bbd54db6a46b0630f43e7126f1a591c0a17
P.C. Shyamshankar <sykora@lucentbeing.com>
2010-09-05 13:51:30 +0530

    Add autofill/exclude of author, created and modified fields in admin.

--- djournal/admin.py
+++ djournal/admin.py
@@ -1,10 +1,21 @@
 '''Admin specification for Djournal.'''
 
+from datetime import datetime
+
 from django.contrib import admin
 
 from djournal.models import Entry
 
 class EntryAdmin(admin.ModelAdmin):
-    pass
+    exclude = ('author', 'created', 'modified')
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.author = request.user
+            obj.created = datetime.now()
+
+        obj.modified = datetime.now()
+
+        obj.save()
 
 admin.site.register(Entry, EntryAdmin)
