fb98cdb210ca91b57bc47899ed9fc94bc00502fa
P.C. Shyamshankar <sykora@lucentbeing.com>
2010-09-17 06:37:54 +0530

    Integrate with django-ripwrap and ReSTinPeace.

--- djournal/models.py
+++ djournal/models.py
@@ -5,6 +5,11 @@ from django.db import models
 
 from djournal import managers
 
+try:
+    from ripwrap.core import formatter
+except ImportError:
+    formatter = lambda text: text
+
 class Entry(models.Model):
     '''Represents a Djournal entry.'''
 
@@ -57,3 +62,9 @@ class Entry(models.Model):
 
     def __unicode__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        self.teaser_html = formatter.render(self.teaser)
+        self.body_html = formatter.render(self.body)
+
+        super(Entry, self).save()
