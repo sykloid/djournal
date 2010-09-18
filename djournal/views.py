ecaf25bcb9e3a2c10f6a300280c891cbeec28b58
P.C. Shyamshankar <sykora@lucentbeing.com>
2010-09-18 15:52:54 +0530

    Add entry_detail view for individual entry pages.

--- djournal/views.py
+++ djournal/views.py
@@ -1,6 +1,6 @@
 '''Views for Djournal.'''
 
-from django.shortcuts import render_to_response
+from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
 
 from djournal.models import Entry
@@ -15,6 +15,21 @@ def entry_index(request, limit=0, template='djournal/entry_index.html'):
 
     context = {
         'entries': entries,
+    }
+
+    return render_to_response(
+        template,
+        context,
+        context_instance=RequestContext(request),
+    )
+
+def entry_detail(request, slug, template='djournal/entry_detail.html'):
+    '''Returns a response of an individual entry, for the given slug.'''
+
+    entry = get_object_or_404(Entry.public, slug=slug)
+
+    context = {
+        'entry': entry,
     }
 
     return render_to_response(
