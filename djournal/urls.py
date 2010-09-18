89237a3b93f4e32aa2b6edcfdb9a145788966a49
P.C. Shyamshankar <sykora@lucentbeing.com>
2010-09-18 15:11:06 +0530

    Alter urls to use entry_index view.

--- djournal/urls.py
+++ djournal/urls.py
@@ -5,14 +5,7 @@ from django.conf.urls.defaults import *
 from djournal.models import Entry
 
 urlpatterns = patterns('',
-    url(r'^$', 'django.views.generic.date_based.archive_index',
-        kwargs={
-            'date_field': 'created',
-            'num_latest': Entry.public.count(),
-            'queryset': Entry.public.all(),
-            'template_name': 'djournal/entry_index.html',
-            'template_object_name': 'entries',
-        },
+    url(r'^$', 'djournal.views.entry_index',
         name='djournal_entry_index',
     ),
     url(r'^(?P<slug>.*)/$',
