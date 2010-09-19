d3d012d0d5d5add4ef77d1fd8c0733f4900aa225
P.C. Shyamshankar <sykora@lucentbeing.com>
2010-09-19 17:01:50 +0530

    Add feed class for entries with a given tag.

--- djournal/feeds.py
+++ djournal/feeds.py
@@ -2,8 +2,11 @@
 
 from django.contrib.syndication.views import Feed
 from django.core.urlresolvers import reverse
+from django.shortcuts import get_object_or_404
 from django.template import Context
 from django.template.loader import get_template
+
+from taggit.models import Tag
 
 from djournal.models import Entry
 
@@ -21,6 +24,33 @@ class EntryFeed(Feed):
 
     def items(self):
         return Entry.public.all()
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return item.teaser_html
+
+    def item_link(self, item):
+        return item.get_absolute_url()
+
+class TaggedEntryFeed(Feed):
+    def get_object(self, request, slug):
+        return get_object_or_404(Tag, slug=slug)
+
+    def items(self, obj):
+        return Entry.public.filter(tags__in=[obj])
+
+    def title(self, obj):
+        template = get_template('djournal/feeds/tagged_entry_feed_title.html')
+        return template.render(Context({'tag': obj}))
+
+    def description(self, obj):
+        template = get_template('djournal/feeds/tagged_entry_feed_description.html')
+        return template.render(Context({'tag': obj}))
+
+    def link(self, obj):
+        return reverse('djournal_tagged_entry_index', kwargs={'slug': obj.slug})
 
     def item_title(self, item):
         return item.title
